f2598114e510ed2fd1083c5fe90a5d8a6de46635
Swati Pal <swatipal39@gmail.com>
2024-03-19 00:12:27 +0530

    Changes to AboutUs Section

--- stackbid/src/App.js
+++ stackbid/src/App.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import AboutUs from './Components/AboutUs/AboutUs';
-import SellerHistory from './Components/SellerHistory/SellerHistory'; // Import SellerHistory component
+//import SellerHistory from './Components/SellerHistory/SellerHistory'; // Import SellerHistory component
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {/* Render SellerHistory component */}
-        
-        <SellerHistory/>
+        <AboutUs/>
       </header>
     </div>
   );
--- stackbid/src/Components/AboutUs/AboutUs.js
+++ stackbid/src/Components/AboutUs/AboutUs.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import AboutUsImage from './AboutUs.png';
+import './AboutUs.css';
 
 const AboutUs = () => {
   return (
     <div className="about-us-container">
       <div className="about-us-text">
-        <h2>StackBid: Bid Smarter, Win Better, Experience Excellence!</h2>
+        <h2><span className="StackBidLarge">StackBid</span>: Bid Smarter, Win Better, Experience Excellence!</h2>
         <h3>About Us</h3>
         <p className="about-us-content">
           We are a team of motivated individuals who identified a gap and absence of a plaform that connects the seller and bidder with different needs. We found the absence of a generalised auction platform which is not specific to cater a single category only but serve the demands from diverse array. Our mission is to provide seller and bidder a plaform where they can experience transparency and excellence together.
