160e9e64a08d449fa57f5120f28ee6472e404807
shwetasng <shwetasng17@gmail.com>
2024-03-19 10:46:15 +0530

    Navbar folder created

---
+++ stackbid/src/Components/AboutUs/Navbar/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div className="logo">Logo Placeholder</div>
+      <ul>
+        <li>About Us</li>
+        <li>Dashboard</li>
+        <li>Feedback</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
