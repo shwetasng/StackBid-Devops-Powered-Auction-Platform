701f8612c1c3dba373243a5cb0a3b2c6e28c869b
Yadrishi <dixityadrishi@gmail.com>
2024-04-02 21:58:04 +0530

    added

--- stackbid/src/App.js
+++ stackbid/src/App.js
@@ -1,17 +1,23 @@
 import React from "react";
 import Navbar from "./Components/Navbar/Navbar";
 import AboutUs from "./Components/AboutUs/AboutUs";
-import Login from "./Components/login/login";
-import Feedback from "./Components/feedback/feedback";
 import SellerHistory from "./Components/SellerHistory/SellerHistory"; // Import SellerHistory component
 import "./App.css";
 import UploadItem from "./Components/UploadItem/UploadItem";
-
+import Feedback from "./Components/feedback/feedback";
+import login from "./Components/login";
+import History from "./Components/History/history";
+import Cart from "./Components/Cart/cart";
+import Profile from "./Components/Profile/profile";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Login />
+        {/*<login /> */}
+        {/*< History / >*/}
+        {/*<Cart />*/}
+
+        {/*<Profile /> */}
         {/* <Feedback /> */}
         {/* <UploadItem /> */}
         {/* <Navbar/> */}
---
+++ stackbid/src/Components/History/history.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './History.css'; // Import the CSS file for styling
+
+const History = ({ bidHistory }) => {
+  return (
+    <div className="history-container">
+      <h2>Bid History</h2>
+      <div className="bid-list">
+        {bidHistory.map((bid, index) => (
+          <div key={index} className="bid-item">
+            <div>Item: {bid.itemName}</div>
+            <div>Bid Amount: ${bid.bidAmount}</div>
+            <div>Status: {bid.status}</div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default History;
